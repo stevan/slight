5608faeeaf796f559e7d1aff5e70a9cc0cfc40fd
Stevan Little <stevan.little@gmail.com>
2025-12-14 21:59:09 +0100

    foo

--- slight.ts
+++ slight.ts
@@ -10,13 +10,13 @@ abstract class Term {
 }
 
 class Nil extends Term {
-    toNativeStr () : string { return '()' }
+    toNativeStr () : string { return 'nil' }
 }
 
 class Bool extends Term {
     public value : boolean;
 
-    constructor(value : boolean) {
+    constructor (value : boolean) {
         super();
         this.value = value;
     }
@@ -27,7 +27,7 @@ class Bool extends Term {
 class Num  extends Term {
     public value : number;
 
-    constructor(value : number) {
+    constructor (value : number) {
         super();
         this.value = value;
     }
@@ -38,18 +38,18 @@ class Num  extends Term {
 class Str  extends Term {
     public value : string;
 
-    constructor(value : string) {
+    constructor (value : string) {
         super();
         this.value = value;
     }
 
-    toNativeStr () : string { return `"${this.value}"` }
+    toNativeStr () : string { return this.value }
 }
 
 class Sym  extends Term {
     public ident : string;
 
-    constructor(ident : string) {
+    constructor (ident : string) {
         super();
         this.ident = ident;
     }
@@ -61,7 +61,7 @@ class Pair extends Term {
     public first  : Term;
     public second : Term;
 
-    constructor(fst : Term, snd : Term) {
+    constructor (fst : Term, snd : Term) {
         super();
         this.first  = fst;
         this.second = snd;
@@ -76,20 +76,21 @@ abstract class List<T extends Term> extends Term {
     public items  : T[];
     public offset : number;
 
-    constructor(items : T[], offset : number = 0) {
+    constructor (items : T[], offset : number = 0) {
         super();
         this.items  = items;
         this.offset = offset;
     }
 
-    AT (i : number) : T | undefined {
+    at (i : number) : T {
+        if ((this.offset + i) > this.items.length) throw new Error('OVERFLOW!');
         return this.items[ this.offset + i ];
     }
 
-    MAP<U> (f : (i : T) => U) : U[] {
+    mapItems<U> (f : (i : T) => U) : U[] {
         let list = [];
         for (let i = 0; i < this.length; i++) {
-            list.push( f( this.AT(i) ) );
+            list.push( f( this.at(i) ) );
         }
         return list;
     }
@@ -111,7 +112,7 @@ class Cons extends List<Term> {
     }
 
     map (f : (i : Term) => Term) : Cons {
-        return new Cons( this.MAP<Term>(f) );
+        return new Cons( this.mapItems<Term>(f) );
     }
 }
 
@@ -121,8 +122,116 @@ class PairList extends List<Pair> {
         return new PairList(this.items, this.offset + 1);
     }
 
-    map (f : (i : Pair) => Pair) : Cons {
-        return new PairList( this.MAP<Pair>(f) );
+    map (f : (i : Pair) => Pair) : PairList {
+        return new PairList( this.mapItems<Pair>(f) );
+    }
+}
+
+class Binding extends Pair {
+    public first : Sym;
+
+    constructor (sym : Sym, value : Term) {
+        super(sym, value);
+    }
+
+    toNativeStr () : string {
+        return `(${this.first.toNativeStr()} : ${this.second.toNativeStr()})`
+    }
+}
+
+class Lambda extends Term {
+    public params : Cons;
+    public body   : Cons;
+
+    constructor (params : Cons, body : Cons) {
+        super();
+        this.params = params;
+        this.body   = body;
+    }
+
+    toNativeStr () : string {
+        return `(& ${this.params.toNativeStr()} ${this.body.toNativeStr()})`
+    }
+}
+
+class Closure extends Term {
+    public lambda : Lambda;
+    public env    : Environment;
+
+    constructor (lambda : Lambda, env : Environment) {
+        super();
+        this.lambda = lambda;
+        this.env    = env;
+    }
+
+    // TODO - capture closed over vars for toNativeStr
+
+    toNativeStr () : string {
+        return `(@ ${this.lambda.toNativeStr()} %())`
+    }
+}
+
+type NativeFunc = (params : Term[], env : Environment) => Term;
+
+class Native extends Term {
+    public name : string;
+    public body : NativeFunc;
+
+    constructor (name : string, body : NativeFunc) {
+        super();
+        this.name = name;
+        this.body = body;
+    }
+
+    toNativeStr () : string {
+        return `(* ${this.name})`
+    }
+}
+
+interface Builtins {
+    [name : string] : Native;
+}
+
+class Environment extends List<Binding> {
+    public builtins : Builtins;
+
+    constructor (items : Binding[], builtins : Builtins, offset : number = 0) {
+        super(items, offset);
+        this.builtins = builtins;
+    }
+
+    get tail () : Environment | Nil {
+        if (this.length == 1) return new Nil();
+        return new Environment(this.items, this.builtins, this.offset + 1);
+    }
+
+    map (f : (i : Binding) => Binding) : Environment {
+        return new Environment( this.mapItems<Binding>(f), this.builtins );
+    }
+
+    lookup (sym : Sym) : Term {
+        if (this.builtins.hasOwnProperty(sym.ident)) {
+            return this.builtins[sym.ident];
+        }
+
+        for (let i = 0; i < this.length; i++) {
+            let binding = this.at(i);
+            if (binding.first.ident == sym.ident) {
+                return binding.second;
+            }
+        }
+        return new Nil();
+    }
+
+    derive (bindings : Binding[]) {
+        return new Environment(
+            [
+                ...bindings,
+                ...this.items.slice(this.offset),
+            ],
+            this.builtins,
+            this.offset,
+        )
     }
 }
 
@@ -132,7 +241,7 @@ class PairList extends List<Pair> {
 
 type ParseExpr = Term | ParseExpr[];
 
-function parse (source : string) : Term {
+function parse (source : string) : ParseExpr {
     const SPLITTER = /'(?:[^'\\]|\\.)*'|[()]|[^\s()']+/g
 
     const tokenize = (src : string) : string[] => src.match(SPLITTER) ?? [];
@@ -157,40 +266,145 @@ function parse (source : string) : Term {
         return parseList( remaining, [ ...acc, expr ] );
     }
 
-    const buildTree = (expr : ParseExpr) : Term  => {
-        if (!Array.isArray(expr)) return expr;
-        if (expr.length == 3) {
-            let [ fst, sym, snd ] = expr;
-            if (sym instanceof Sym && sym.ident == '.') {
-                return new Pair( buildTree(fst), buildTree(snd) );
+    let [ expr, rest ] = parseTokens( tokenize( source ) );
+    return expr;
+}
+
+function compile (expr : ParseExpr, builtins : Builtins) : Term {
+    if (!Array.isArray(expr)) return expr;
+
+    if (expr.length == 0) return new Cons([]);
+
+    let rest = expr.map((e) => compile(e, builtins));
+
+    // handle pairs and bindings
+    if (rest.length == 3) {
+        let [ fst, sym, snd ] = rest;
+        if (sym instanceof Sym) {
+            switch(sym.ident) {
+            case '.' : return new Pair( fst, snd );
+            case ':' :
+                if (!(fst instanceof Sym)) throw new Error(`Bindings must start with Sym not ${fst.constructor}`);
+                return new Binding( fst, snd );
+            default:
+                // let it fall through
             }
-        }
-        let body = expr.map(buildTree);
-        if (body.every((p) => p instanceof Pair)) {
-            return new PairList( body );
-        } else {
-            return new Cons( body );
         }
     }
 
-    let [ expr, rest ] = parseTokens( tokenize( source ) );
-    return buildTree( expr );
+    // handle special forms
+    if (rest[0] instanceof Sym) {
+        switch (rest[0].ident) {
+        case 'lambda':
+            let params = rest[1];
+            let body   = rest[2];
+            if (!(params instanceof Cons)) throw new Error(`Lambda params must be a Cons not ${params.constructor}`);
+            if (!(body   instanceof Cons)) throw new Error(`Lambda body must be a Cons not ${body.constructor}`);
+            return new Lambda( params, body );
+        default:
+            // let it fall through
+        }
+    }
+
+    // handle different list types ...
+    if (rest.every((p) => p instanceof Binding)) {
+        return new Environment( rest, builtins );
+    }
+    else if (rest.every((p) => p instanceof Pair)) {
+        return new PairList( rest );
+    }
+    else {
+        return new Cons( rest );
+    }
 }
 
-let ast = parse(`
-    (1 2 3 4 5)
-`);
 
-console.log(ast.toNativeStr());
-console.log(ast);
+// -----------------------------------------------------------------------------
 
-let ast2 = ((ast as Cons).tail as Cons).map((p) => new Num( (p as Num).value * 10 ) );
+const liftNumBinOp = (f : (n : number, m : number) => number) : NativeFunc => {
+    return (args : Term[], env : Environment) => {
+        let [ lhs, rhs ] = args;
+        if (!(lhs instanceof Num)) throw new Error(`LHS must be a Num, not ${lhs.constructor}`);
+        if (!(rhs instanceof Num)) throw new Error(`RHS must be a Num, not ${rhs.constructor}`);
+        return new Num( f(lhs.value, rhs.value) );
+    }
+}
 
-console.log(ast2.toNativeStr());
-console.log(ast2);
+const liftNumCompareOp = (f : (n : number, m : number) => boolean) : NativeFunc => {
+    return (args : Term[], env : Environment) => {
+        let [ lhs, rhs ] = args;
+        if (!(lhs instanceof Num)) throw new Error(`LHS must be a Num, not ${lhs.constructor}`);
+        if (!(rhs instanceof Num)) throw new Error(`RHS must be a Num, not ${rhs.constructor}`);
+        return new Bool( f(lhs.value, rhs.value) );
+    }
+}
 
-console.log((ast2 as Cons).tail.toNativeStr());
-console.log((ast2 as Cons).tail);
+
+let env = compile(
+    parse(`(
+        (add  : (lambda (x y) (+ x y)))
+        (main :
+
+            (add 10 20)
+
+        )
+    )`),
+    {
+        '+'  : new Native('+',  liftNumBinOp((n, m) => n + m)),
+        '-'  : new Native('-',  liftNumBinOp((n, m) => n - m)),
+        '*'  : new Native('*',  liftNumBinOp((n, m) => n * m)),
+        '/'  : new Native('/',  liftNumBinOp((n, m) => n / m)),
+        '%'  : new Native('%',  liftNumBinOp((n, m) => n % m)),
+
+        '>=' : new Native('>=', liftNumCompareOp((n, m) => n >= m)),
+        '>'  : new Native('>',  liftNumCompareOp((n, m) => n >  m)),
+        '<=' : new Native('<=', liftNumCompareOp((n, m) => n <= m)),
+        '<'  : new Native('<',  liftNumCompareOp((n, m) => n <  m)),
+        '==' : new Native('==', liftNumCompareOp((n, m) => n == m)),
+        '!=' : new Native('!=', liftNumCompareOp((n, m) => n != m)),
+    }
+) as Environment;
+
+let program = env.map((b) => {
+    if (b.second instanceof Lambda) {
+        return new Binding( b.first, new Closure( b.second as Lambda, env ) );
+    }
+    return b;
+});
+
+console.log(program.toNativeStr());
+
+function evaluate (expr : Term, env : Environment) : Term {
+    switch (expr.constructor) {
+    case Nil    :
+    case Num    :
+    case Str    :
+    case Bool   :
+    case Pair   :
+    case Native : return expr;
+    case Sym    : return env.lookup( expr as Sym );
+    case Lambda : return new Closure( expr as Lambda, env );
+    case Cons   :
+        let [ call, ...args ] = (expr as Cons).mapItems<Term>((e) => evaluate(e, env));
+        switch (call.constructor) {
+        case Native  : return (call as Native).body(args, env);
+        case Closure :
+            let local  = (call as Closure).env;
+            let lambda = (call as Closure).lambda;
+            let bindings = [];
+            for (let i = 0; i < args.length; i++) {
+                bindings[i] = new Binding( lambda.params.at(i) as Sym, args[i] );
+            }
+            return evaluate( lambda.body, local.derive(bindings) );
+        default:
+            throw new Error(`Must be Native or Closure, not ${call.constructor}`);
+        }
+    }
+}
+
+let result = evaluate(program.lookup(new Sym('main')), program);
+console.log(result);
+console.log(result.toNativeStr());
 
 
 
